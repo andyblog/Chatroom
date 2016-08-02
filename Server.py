bd2d14ff9be5204e6f6501e8925a3ed72fbcdc5e
WiseDoge <wisedoge@outlook.com>
2016-08-02 23:09:03 +0800

    Modify the server

--- Server.py
+++ Server.py
@@ -1,4 +1,6 @@
 import redis
+import sqlite3
+import threading
 
 # 服务器端。
 conn = redis.Redis()
@@ -14,5 +16,39 @@ def pub():
                    '----------------------------------'
         conn.publish("chat", real_mes.encode("utf-8"))
 
-if __name__ == '__main__':
+def storeMesLog(raw_info):
+    c = sqlite3.connect("meslog.db")
+    cur = c.cursor()
+    if 'Administrator say:' in raw_info:
+        return 
+
+    raw_info_list = raw_info.lower().split(':')
+    name = raw_info_list[0].replace(" say", "")
+    messsage = ''.join(raw_info_list[1:])
+    cur.execute("CREATE TABLE IF NOT EXISTS " + name +\
+     " (id INTEGER PRIMARY KEY AUTOINCREMENT, content text, CreatedTime TimeStamp NOT NULL DEFAULT CURRENT_TIMESTAMP)")
+
+    cur.execute("INSERT INTO " + name + " (content) VALUES (?)" , (messsage,))
+    c.commit()
+
+    cur.close()
+    c.close()
+
+def sub():
+    global conn
+    sub = conn.pubsub()
+    sub.subscribe(["chat"])
+    for msg in sub.listen():
+        if msg['data'] == 1:
+            pass
+        else:
+            storeMesLog(msg['data'].decode('utf-8'))
+
+def run_server():
+    sub_thread = threading.Thread(target=sub)
+    sub_thread.setDaemon(True)
+    sub_thread.start()
     pub()
+
+if __name__ == '__main__':
+    run_server()
